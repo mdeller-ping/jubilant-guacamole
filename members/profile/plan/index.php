91e6990604c45899119a746a7ba32867fc23db1c
mdeller-ping <mdeller@pingidentity.com>
2020-07-28 09:57:28 -0400

    tweaks

--- members/profile/plan/index.php
+++ members/profile/plan/index.php
@@ -135,8 +135,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <script>
-        let authHeader = 'Y249YWRtaW5pc3RyYXRvcjoyRmVkZXJhdGVNMHJl';
-        let pingDirectory = 'auth.tu.demoenvi.com:1443'
+        let authHeader = 'Y249ZGlyZWN0b3J5IG1hbmFnZXI6MkZlZGVyYXRlTTByZQ==';
+        let pingDirectory = 'dir.tu.demoenvi.com:8443'
         let distinguishedName = '<?php echo $_SERVER['HTTP_X_PA_DN'] ?>';
 
         function updateAccount() {
