4453772dfdb5cdea27ff138d47f515752f18e10e
mdeller-ping <mdeller@pingidentity.com>
2020-08-03 07:48:49 -0400

    pulled from prod

--- api/score/index.php
+++ api/score/index.php
@@ -1,6 +1,12 @@
 <?php
 header('Content-Type: application/json');
-$myObj->transUnionScore = rand (300, 830);
+$seedNumber = rand (320, 830);
+$rangeHigh = $seedNumber + 20;
+$rangeLow = $seedNumber - 20;
+
+$myObj->TransUnionScore = rand ($rangeLow, $rangeHigh);
+$myObj->ExperianScore = rand ($rangeLow, $rangeHigh);
+$myObj->EquifaxScore = rand ($rangeLow, $rangeHigh);
 $myJSON = json_encode($myObj);
 echo $myJSON;
 ?>
